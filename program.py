a59748a1087e612ae96abbaebf98c24670a6d008
Daniel Karpelevitch <daniel@karpelevitch.net>
2020-07-27 21:36:35 -0700

    umm... too many changes to count. It finally works!!

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import smartsheet
+
 from FY_Q_sort import calc_fy_q_hardcoded
 
 smart = smartsheet.Smartsheet()  # use 'SMARTSHEET_ACCESS_TOKEN' env variable
@@ -8,84 +9,122 @@ REQUEST_SHEET_ID = 4026093033285508
 MAP_SHEET_ID = 8844668382275460
 
 
-def process_sheet(sheet_id, simulate=False):  # main loop for processing the sheet
-    rows_to_update = []
-    sheet = smart.Sheets.get_sheet(sheet_id)
-    rows = sheet.rows
-    col_ids_names = get_columns_id_and_title(sheet_id)
-    print_col_headings(col_ids_names)
+def process_sheet(request_sheet_id, map_sheet_id, simulate=False):  # main loop for processing the sheet
+    rows = smart.Sheets.get_sheet(request_sheet_id).rows
+    column_mapping = column_name_to_id_map(request_sheet_id)
+    print_col_headings(column_mapping)
     for row in rows:
         if check_row(row):
             print_row(row)
-            send_row(row)
-
-            new_row = update_row_status(row, color='Green')
-            if new_row:  # checks that the new row was updated successfully
-                rows_to_update.append(new_row)
-    if not simulate:
-        smart.Sheets.update_rows(sheet_id, rows_to_update)
-    else:
-        print('\nSimulation! These rows would have been updated to green:')
-        for row in rows_to_update:
-            print(str(row.id).ljust(20), row.cells[1].value)
+            if not simulate:
+                send_row(sheet_id=map_sheet_id,
+                         row=row,
+                         request_column_mapping=column_mapping)
+                smart.Sheets.update_rows(request_sheet_id,
+                                         update_row_status(row=row,
+                                                           color='Green'))
+            else:
+                print('Simulation! This row would have been updated to green and added to the map sheet.\n')
 
 
-def get_columns_id_and_title(sheet_id):  # returns a list of (id, title) tuples of all columns in sheet
-    columns = smart.Sheets.get_columns(
-        sheet_id,
-        include_all=True).data
-    col_ids_names = [(column.id, column.title) for column in columns]
-    return col_ids_names
+def send_row(sheet_id, row, request_column_mapping):
+    fy, q = calc_fy_q_hardcoded(get_cell_by_column_name(row=row,
+                                                        column_name='Event Start Date',
+                                                        col_map=request_column_mapping).value)
+    fy_q_dict = make_fy_q_dict(sheet_id)
 
+    new_row = smartsheet.models.Row()
+    new_row.parent_id = get_quarter_parentid(fy, q, fy_q_dict)
+    new_row.to_bottom = True
 
-def print_col_headings(cols):  # prints the column name and id for the 2 - 7th column
-    print(*(str(col[1]).ljust(24) for col in cols[1:6]), 'Start FY/Quarter')
-    print(*(str(col[0]).ljust(24) for col in cols[1:6]), end='\n\n')
+    map_column_mapping = column_name_to_id_map(sheet_id)
+
+    for cell in row.cells:
+        if reverse_dict_search(request_column_mapping, cell.column_id) in map_column_mapping.keys():
+            new_cell = smartsheet.models.Cell()
+            new_cell.value = cell.value
+            new_cell.column_id = map_column_mapping[reverse_dict_search(request_column_mapping, cell.column_id)]
+            new_row.cells.append(new_cell)
+
+    smart.Sheets.add_rows(sheet_id, new_row)
 
 
 def check_row(row, index=0, val_to_test='Yellow'):  # checks if the given cell in the given row is the given value
     return row.cells[index].value == val_to_test
 
 
+def column_name_to_id_map(sheet_id):  # returns a title:id dict of all columns in sheet
+    return {column.title: column.id for column in smart.Sheets.get_columns(sheet_id, include_all=True).data}
+
+
+def get_cell_by_column_name(row, column_name, col_map):
+    return row.get_column(col_map[column_name])  # {NAME: ID}
+
+
+def print_col_headings(cols):  # prints the column name and id for the 2 - 7th column
+    print(*(column_format(col_title) for col_title in cols.keys()), 'Start FY/Quarter')
+    print(*(str(col_id).ljust(23) for col_id in cols.values()), end='\n\n')
+
+
 def print_row(row):  # prints the 2 - 7th column in the passed-in row, + FY/Quarter with some fancy formatting
-    print(*((str(cell.value)[:22] + (str(cell.value)[22:] and '..')).ljust(24) for cell in row.cells[1:6]),
-          sep=' ', end=' ')
+    print(*(column_format(cell.display_value or cell.value) for cell in row.cells), sep=' ', end=' ')
     fy, q = calc_fy_q_hardcoded(row.cells[3].value)
     print(f'FY{fy} Q{q}')
 
 
-def send_row(row):
-    pass
+def column_format(item, just=23):
+    return (str(item)[:just - 2] + (str(item)[just - 2:] and '..')).ljust(just)
+
+
+def reverse_dict_search(search_dict, search_value):
+    for key, val in search_dict.items():
+        if val == search_value:
+            return key
+
+
+def make_fy_q_dict(sheet_id):
+    fy_q_dict = {str(fy.cells[0].value): [fy, {}] for fy in find_fy_rows(sheet_id)}
+    for year, (year_row, _) in fy_q_dict.items():
+        quarters = find_quarter_rows(sheet_id, year_row)
+        fy_q_dict[year][1] = {quarter.cells[0].value: quarter for quarter in quarters}
+    return fy_q_dict
+
+
+def find_fy_rows(sheet_id):
+    fy_rows = []
+    rows = smart.Sheets.get_sheet(sheet_id).rows
+    for row in rows:
+        if not row.to_dict().get('parentId', False):  # checks if row has a porent
+            fy_rows.append(row)
+    return fy_rows
+
+
+def find_quarter_rows(sheet_id, year_row):
+    q_rows = []
+    rows = smart.Sheets.get_sheet(sheet_id).rows
+    for row in rows:
+        if row.to_dict().get('parentId', False) == year_row.id:  # checks if row's parent is the FY
+            q_rows.append(row)
+    return q_rows
+
+
+def get_quarter_parentid(fy, q, fy_q_dict):
+    return fy_q_dict['FY' + str(fy)][1]['Q' + str(q)].id
 
 
 def update_row_status(row, color='Green'):
     # creates a new row object with the old row's id and cells, updates the first cell to the passed in color
+    allowed_colors = ['Red', 'Yellow', 'Green']
     new_row = smartsheet.models.Row()
     new_row.id, new_row.cells = row.id, row.cells
     try:
-        assert color in ['Red', 'Yellow', 'Green']
+        assert color in allowed_colors
         new_row.cells[0].value = color
+        return new_row
     except AssertionError:
-        print(f"Color must be one of: 'Red', 'Yellow', 'Green'. Color was {color}")
-        return None
-    except Exception as e:
-        print(e)
-        return None
-    return new_row
-
-# make a new requests sheet with added color dot column
-# make a new map sheet for testing with original data, create fake data in requests and test adding it to the sheet
-# pull in data from test requests sheet, print out new (yellow) rows with calculated FY/Q
-
-# red = don't add, not added, yellow = add, green = don't add, already added
-
-# implement fiscal year separations per Cisco calendar
-# on run transfer of new row/information of a show,
-# check if color circle is yellow
-# find FY and Quarter row, create if doesn't exist
-# add row to right spot within quarter
-# change circle color to green
+        print(f"Color must be one of: {allowed_colors}. Color was {color}. Row will not be updated")
+    return row
 
 
 if __name__ == '__main__':
-    process_sheet(REQUEST_SHEET_ID, simulate=True)
+    process_sheet(REQUEST_SHEET_ID, MAP_SHEET_ID, simulate=False)
