fea3f9a7376ecbc248112a9368a69601b1cc1cd6
Daniel Karpelevitch <daniel@karpelevitch.net>
2020-08-12 15:36:51 -0700

    optimized further, colorize.py  now only makes a single call to update rows

--- colorize.py
+++ colorize.py
@@ -10,11 +10,9 @@ quarter_and_event_colors = [(12, 5),
 
 def colorize_rows(smart: smartsheet.Smartsheet, sheet_id: int) -> None:
     from program import find_event_rows, find_fy_rows, find_quarter_rows
-    year_rows_to_update = []
+    rows_to_update = []
     for year_row in find_fy_rows(sheet_id):
-        quarter_rows_to_update = []
         for i, quarter_row in enumerate(find_quarter_rows(sheet_id, year_row)):
-            event_rows_to_update = []
             for event_row in find_event_rows(sheet_id, quarter_row.id):
                 new_row = smartsheet.models.Row(dict(id=event_row.id,
                                                      cells=event_row.cells))
@@ -24,8 +22,7 @@ def colorize_rows(smart: smartsheet.Smartsheet, sheet_id: int) -> None:
                     if not cell.value:
                         cell.value = ''
                 new_row.format = f',,,,,,,,,{quarter_and_event_colors[i][1]},,,,,,'
-                event_rows_to_update.append(new_row)
-            smart.Sheets.update_rows(sheet_id, event_rows_to_update)
+                rows_to_update.append(new_row)
 
             new_row = smartsheet.models.Row(dict(id=quarter_row.id,
                                                  cells=quarter_row.cells))
@@ -35,8 +32,7 @@ def colorize_rows(smart: smartsheet.Smartsheet, sheet_id: int) -> None:
                 if not cell.value:
                     cell.value = ''
             new_row.format = f',,,,,,,,,{quarter_and_event_colors[i][0]},,,,,,'
-            quarter_rows_to_update.append(new_row)
-        smart.Sheets.update_rows(sheet_id, quarter_rows_to_update)
+            rows_to_update.append(new_row)
 
         new_row = smartsheet.models.Row(dict(id=year_row.id,
                                              cells=year_row.cells))
@@ -46,5 +42,5 @@ def colorize_rows(smart: smartsheet.Smartsheet, sheet_id: int) -> None:
             if not cell.value:
                 cell.value = ''
         new_row.format = f',,,,,,,,,{year_row_color_index},,,,,,'
-        year_rows_to_update.append(new_row)
-    smart.Sheets.update_rows(sheet_id, year_rows_to_update)
+        rows_to_update.append(new_row)
+    smart.Sheets.update_rows(sheet_id, rows_to_update)
