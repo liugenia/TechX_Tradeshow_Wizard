ab2a1c367863411ac676deef1823c9efae7aab2e
Daniel Karpelevitch <daniel@karpelevitch.net>
2020-08-31 23:16:49 -0700

    Made the call to program.py start on a separate thread

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import threading
+
 from flask import Flask, request, Response
 
 from program import MAP_SHEET_ID, process_sheet, REQUEST_SHEET_ID
@@ -18,8 +20,9 @@ def webhook():
         response.headers['Smartsheet-Hook-Challenge'] = request.headers['Smartsheet-Hook-Challenge']
     elif request.json:
         if 'events' in request.json:
-            print('Running program.py')  # todo: add multithreading so we don't wait for program.py to finish
-            process_sheet(REQUEST_SHEET_ID, MAP_SHEET_ID)
+            print('Running program.py')
+            threading.Thread(target=process_sheet, args=[REQUEST_SHEET_ID, MAP_SHEET_ID]).start()
+
     return response
 
 
